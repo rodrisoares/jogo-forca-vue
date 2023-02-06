c9baa2d279df5eb1eb645ffa56640e3a2b531449
Rodrigo Soares <diihhsoares2@gmail.com>
2023-02-06 18:14:11 -0300

    criando componentes do jogo

---
+++ src/components/CorpoForca.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="forca">
+    <img 
+      :src="`https://raw.githubusercontent.com/rodrisoares/jogoForcaVue/main/src/assets/${erros}.svg`"
+      alt="Imagem de uma forca"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CorpoForca",
+  props: {
+    erros: Number,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style>
+.forca img {
+  height: 250px;
+}
+</style>
---
+++ src/components/InicioJogo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="jogo">
+    <CorpoForca :erros="erros" />
+
+    <PalavraJogo
+      :palavra="palavra"
+      :dica="dica"
+      :verificarLetra="verificarLetra"
+      :etapa="etapa"
+    />
+
+    <TecladoJogo
+      v-if="etapa === 'jogo'"
+      :letras="letras"
+      :verificarLetra="verificarLetra"
+      :jogar="jogar"
+    />
+
+    <FimJogo
+      v-if="etapa != 'jogo'"
+      :etapa="etapa"
+      :texto="etapa === 'ganhador' ? 'Acertou :)' : 'Errou, fim de jogo :('"
+      :jogarNovamente="jogarNovamente"
+    />
+  </div>
+</template>
+
+<script>
+import CorpoForca from "./CorpoForca.vue";
+import PalavraJogo from "./PalavraJogo.vue";
+import TecladoJogo from "./TecladoJogo.vue";
+import FimJogo from "./FimJogo.vue";
+
+export default {
+  name: "InicioJogo",
+  props: {
+    erros: Number,
+    palavra: String,
+    dica: String,
+    verificarLetra: Function,
+    etapa: String,
+    letras: Array,
+    jogar: Function,
+    jogarNovamente: Function,
+  },
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    CorpoForca,
+    PalavraJogo,
+    TecladoJogo,
+    FimJogo,
+  },
+};
+</script>
+
+<style>
+.jogo {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+</style>
