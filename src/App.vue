a7dd0aa781fce568a7e064f52ead378f2093fa86
Rodrigo Soares <diihhsoares2@gmail.com>
2023-02-06 18:12:03 -0300

    criando estrutura do jogo

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <h1>Jogo da Forca Vue.JS</h1>
+
+    <section id="inicio" v-if="tela === 'inicio'">
+     
+    </section>
+
+    <section id="jogo" v-if="tela === 'jogo'">
+      
+    </section>
+  </div>
+</template>
+
+<script>
+import "./css/global.css";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      tela: "inicio",
+    };
+  },
+  components: {
+  
+  },
+  methods: {
+  
+  },
+};
+</script>
+
+<style>
+#app {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
